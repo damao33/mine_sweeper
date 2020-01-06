c211f5765485fb1cea45166e777acd00e6dea0c6
81 <1289209158@qq.com>
2020-01-06 10:25:18 +0800

    注册时两次密码输入不一样提示

--- src/gui/Registry.java
+++ src/gui/Registry.java
@@ -81,6 +81,70 @@ import java.awt.event.*;
 		con.add(Box.createHorizontalStrut(30000));
 		
 		regBtn=new JButton("注册");
+		
+		regBtn.addActionListener(new ActionListener() {
+            /*@Override
+            public void widgetSelected(SelectionEvent arg0) {
+            	
+            	JDBC jdbc=new JDBC();
+                jdbc.connect();
+                try {
+                    
+                    String acount=txtName.getText();
+                    //注册时2次输入的密码相同时才把密码设置到user信息中
+                    String pwd1=String.valueOf(txtPass.getPassword());
+                    String pwd2=String.valueOf(txtRPass.getPassword());
+                    boolean isWrong=true;
+                    if(isWrong){
+                        if((pwd1.equals(pwd2))){
+                        	User ru=new User(acount,pwd1);
+                            isWrong=false;
+                        }
+                        else
+                        {
+                            JOptionPane.showMessageDialog(null, "密码不一致,请再次输入密码");
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                	e.printStackTrace();
+                	
+                };
+            }*/
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				//JDBC jdbc=new JDBC();
+                //jdbc.connect();
+                try {
+                    
+                    String acount=txtName.getText();
+                    //注册时2次输入的密码相同时才把密码设置到user信息中
+                    String pwd1=String.valueOf(txtPass.getPassword());
+                    String pwd2=String.valueOf(txtRPass.getPassword());
+                    boolean isWrong=true;
+                    if(isWrong){
+                        if((pwd1.equals(pwd2))){
+                        	User ru=new User(acount,pwd1);
+                            isWrong=false;
+                        }
+                        else
+                        {
+                            JOptionPane.showMessageDialog(null, "密码不一致,请再次输入密码");
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                	ex.printStackTrace();
+                	
+                };
+			}
+		});
+            
+                    
 		regBtn.addActionListener(new ActionListener() {
 			
 			@Override
@@ -128,5 +192,6 @@ import java.awt.event.*;
 			login.mainJFrame.setVisible(true);
 		}
 	}
+	
 }
 
